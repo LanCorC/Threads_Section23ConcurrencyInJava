da9ec99e75b64723bddef21ac8477103ce25d7b1
LanCorC <lanc3.corcega@gmail.com>
2023-08-09 21:21:48 +1200

    Section 23.392(Concurrency in Java: Threads): introduction to Thread class, uses of Thread subclass, how to make anonymous Thread subclass, IntelliJ tips to color-code thread printouts

---
+++ src/cor/lan/AnotherThread.java
@@ -0,0 +1,10 @@
+package cor.lan;
+
+import static cor.lan.ThreadColor.ANSI_BLUE;
+
+public class AnotherThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println(ANSI_BLUE + "Hello from another thread.");
+    }
+}
--- src/cor/lan/Main.java
+++ src/cor/lan/Main.java
@@ -1,2 +1,21 @@
-package cor.lan;public class Main {
+package cor.lan;
+
+import static cor.lan.ThreadColor.ANSI_GREEN;
+import static cor.lan.ThreadColor.ANSI_PURPLE;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(ANSI_PURPLE + "Hello from the main thread.");
+
+        Thread anotherThread = new AnotherThread();
+        anotherThread.start();
+
+        new Thread() {
+            public void run() {
+                System.out.println(ANSI_GREEN + "Hello from the anonymous class thread");
+            }
+        }.start();
+
+        System.out.println("Hello again from the main thread.");
+    }
 }
